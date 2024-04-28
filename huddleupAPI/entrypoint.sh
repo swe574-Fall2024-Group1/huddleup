80855d0f3c12450a20ce50a3289350187f2a7483
Omer Aslan <32545727+qouv@users.noreply.github.com>
2024-04-28 15:46:04 +0300

    fix: remove collect static

--- huddleupAPI/entrypoint.sh
+++ huddleupAPI/entrypoint.sh
@@ -3,7 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate authAPI --no-input
 python manage.py migrate communityAPI --no-input
-python manage.py collectstatic --no-input
 
 gunicorn huddleupAPI.wsgi:application --bind 0.0.0.0:8000
 
