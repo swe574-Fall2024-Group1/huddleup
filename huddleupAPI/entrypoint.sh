e12f70153af3034a3858b51d83e520f8004af152
Omer Aslan <32545727+qouv@users.noreply.github.com>
2024-04-28 16:56:55 +0300

    fix: update entry point

--- huddleupAPI/entrypoint.sh
+++ huddleupAPI/entrypoint.sh
@@ -4,5 +4,5 @@ python manage.py makemigrations
 python manage.py migrate authAPI --no-input
 python manage.py migrate communityAPI --no-input
 
-gunicorn huddleupAPI.wsgi:application --bind 0.0.0.0:8000
+gunicorn huddleupAPI.wsgi:application --bind huddleup.space:8000
 
