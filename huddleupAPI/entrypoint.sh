7375eac64b33bc5cf08ce0fcebcee04f0e1422f5
Omer Aslan <32545727+qouv@users.noreply.github.com>
2024-04-29 19:08:40 +0300

    fix: add collectstatic

--- huddleupAPI/entrypoint.sh
+++ huddleupAPI/entrypoint.sh
@@ -4,6 +4,8 @@ python manage.py makemigrations authAPI
 python manage.py makemigrations communityAPI
 python manage.py migrate authAPI --no-input
 python manage.py migrate communityAPI --no-input
+python manage.py collectstatic --no-input
+
 
 gunicorn huddleupAPI.wsgi:application --bind 0.0.0.0:8000
 
