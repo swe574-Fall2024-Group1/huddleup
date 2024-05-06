388637bd3fe83d7053d497bd2a2fa4f2a5f5cbc5
Omer Aslan <32545727+qouv@users.noreply.github.com>
2024-05-06 13:58:12 +0300

    revert: use localhost

--- huddleupui/src/hooks/useApi.js
+++ huddleupui/src/hooks/useApi.js
@@ -7,7 +7,7 @@ const useApi = async (url, payload) => {
 
 
   const fetchApi = async () => {
-	const BASE_URL = 'https://huddleup.space:8000'
+	const BASE_URL = 'http://localhost:8000'
 
 	try {
 		const sessionToken = Cookies.get('hudSession')
