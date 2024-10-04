499658b41cac52bac143298bc7e4e41f62f234ca
Aibek Aldabergenov <aaldaber@gmail.com>
2024-10-04 23:53:23 +0300

    settings and requirements changes

--- huddleupAPI/huddleupAPI/settings.py
+++ huddleupAPI/huddleupAPI/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9k_2o*mn24s_9jb6mu2+4n-@m&*_p*di498cfkr!mp+cq8t2gs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 # Application definition
 
@@ -46,14 +44,6 @@ INSTALLED_APPS = [
 	'authAPI.apps.AuthConfig',
 	'communityAPI.apps.CommunityapiConfig',
 ]
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-# ALLOWED HEADERS
-CORS_ALLOW_HEADERS = [
-	'*'
-]
-
 
 MIDDLEWARE = [
 	'corsheaders.middleware.CorsMiddleware',
@@ -94,14 +84,15 @@ WSGI_APPLICATION = 'huddleupAPI.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'djongo',
-		'NAME': 'huddleup',
-		'CLIENT':	{
-			'host': 'somesecret',
-			'authMechanism': 'SCRAM-SHA-1'
-		}
-	}
+    'default': {
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get("POSTGRES_DB", BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get("POSTGRES_USER", "user"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "password"),
+        'HOST': os.environ.get("SQL_HOST", "localhost"),
+        'PORT': os.environ.get("SQL_PORT", "5432"),
+        'ATOMIC_REQUESTS': True,
+    }
 }
 
 
@@ -129,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Istanbul'
 
 USE_I18N = True
 
@@ -139,11 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
