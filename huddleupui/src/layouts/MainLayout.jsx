16c70ae242dc885377c028fc3ccb41210308b68f
erkingonultas <erkingonultas@gmail.com>
2024-10-28 15:06:33 +0300

    feat: mobile home page design

--- huddleupui/src/layouts/MainLayout.jsx
+++ huddleupui/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Layout, Avatar, Card, Button, Modal, Row, Col } from 'antd';
+import { Layout, Avatar, Drawer, Grid, Card, Button, Modal, Row, Col } from 'antd';
 import { UserOutlined, BellOutlined, TeamOutlined, PlusOutlined, UsergroupAddOutlined, NotificationOutlined } from '@ant-design/icons';
 import { useNavigate } from "react-router-dom";
 import useAuth from "../components/Auth/useAuth";
@@ -10,31 +10,44 @@ import RightSidebar from '../components/MainLayout/RighSidebar';
 
 
 const { Header, Content, Footer, Sider } = Layout;
+const { useBreakpoint } = Grid;
 
 export default function MainLayout({ children }) {
     const { userInfo } = useAuth();
     const navigate = useNavigate();
+    const [leftDrawerVisible, setLeftDrawerVisible] = useState(false);
+    const [rightDrawerVisible, setRightDrawerVisible] = useState(false);
+    const toggleRightDrawer = () => setRightDrawerVisible(!rightDrawerVisible);
+
+    const screens = useBreakpoint();
+
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
             <Navbar />
             <Layout>
-                <LeftSidebar />
-                <Layout style={{ padding: '0 24px 24px' }}>
-                    <Content
-                        className="site-layout-background"
-                        style={{
-                            padding: 10,
-                            margin: 0,
-                            minHeight: 280,
-                        }}
-                    >
-                        {children}
-                    </Content>
-                    <Footer className='logo' style={{ textAlign: 'center', fontWeight: 700, color: '#a1a1a1', fontSize: 18 }}>huddleup ©{new Date().getFullYear()}</Footer>
-                </Layout>
-                <RightSidebar />
+            <LeftSidebar />
+            <Layout style={{  padding: screens.md ? '0 24px 24px' :  '44px 24px 24px'}}>
+                <Content
+                    className="site-layout-background"
+                    style={{
+                        padding: 10,
+                        margin: 0,
+                        minHeight: 280,
+                    }}
+                >
+                    {children}
+                </Content>
+                <Footer
+                    className="logo"
+                    style={{ textAlign: 'center', fontWeight: 700, color: '#a1a1a1', fontSize: 18 }}
+                >
+                    huddleup ©{new Date().getFullYear()}
+                </Footer>
             </Layout>
+
+            <RightSidebar />
+        </Layout>
         </Layout>
     );
 }
