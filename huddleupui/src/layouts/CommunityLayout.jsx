e2e80c598199c770817852ec17593b96fbcbadf8
Muharrem Ustaoglu <muharremustaoglu53@gmail.com>
2024-12-08 20:22:03 +0300

    fix:duplicated description fields is removed.

--- huddleupui/src/layouts/CommunityLayout.jsx
+++ huddleupui/src/layouts/CommunityLayout.jsx
@@ -346,19 +346,6 @@ export default function CommunityLayout({ children, allowedUserTypes, canNotMemb
 								</Card>
 
 
-
-
-
-
-
-								<Card
-								    title="Description"
-								    style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px", marginBottom: 15 }}
-								>
-								    <span>{communityInfo ? communityInfo.description : ''}</span>
-								</Card>
-
-
 								<Card title="Description" style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px", marginBottom: 15 }}>
 									<span>{communityInfo ? communityInfo.description : ''}</span>
 								</Card>
